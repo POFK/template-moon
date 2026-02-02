167efae8c07cf917b3a64bdd4e71b383f1f60b84
TX.Mao <mtianxiang@gmail.com>
2026-02-02 20:27:43 +0800

    init: add flake env for both FHS and non-FHS

---
+++ flake.nix
@@ -0,0 +1,79 @@
+{
+  description = "monorepo development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        shellPkgs = pkgs: with pkgs; [
+          proto
+
+          cacert 
+          openssl
+          zlib
+          # 如果你的系统需要编译 Go 代码，可能还需要 gcc/glibc
+          gcc 
+          ];
+
+        shellEnv = ''
+	  #export HTTPS_PROXY=http://10.0.0.11:7890
+	  echo "--- 🛡️  Multi-language Dev Environment Loaded ---"
+	  echo "Available tools:"
+	
+	  if command -v proto &> /dev/null; then
+	    source <(proto activate)
+	    echo "  - proto: $(proto --version)"              
+	  fi
+	
+	  if command -v moon &> /dev/null; then
+	    echo "  - moon: $(moon --version)"              
+	  fi
+	
+	  if [[ -n "$BASH_VERSION" && $- == *i* ]]; then
+	    if type complete &>/dev/null; then
+	      [ -n "$(command -v proto)" ] && source <(proto completions --shell bash)
+	      [ -n "$(command -v moon)" ] && source <(moon completions --shell bash)
+	      echo "  (Autocomplete scripts loaded)"
+	    fi
+	  fi
+	
+	  echo "----------------------------------------------------"
+	'';
+
+        # FHS env
+        myFhs = pkgs.buildFHSEnv {
+          name = "fhs-dev-shell";
+          targetPkgs = shellPkgs;
+	  profile = ''
+          ${shellEnv}
+
+	  export MY_CUSTOM_VAR="fhs-mode"
+	  echo "🚀 Entered FHS environment!"
+	'';
+          runScript = "bash";
+        };
+
+      in
+      {
+        devShells.fhs = myFhs.env;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = shellPkgs pkgs;
+          shellHook = ''
+          ${shellEnv}
+
+	  export MY_CUSTOM_VAR="default-mode"
+	  echo "🚀 Entered non-FHS environment!"
+	'';
+        };
+      }
+    );
+}
